a36a557d1054434e28e38a5e0668c231f3ea53e6
Abhishek <siya@gmail.com>
2025-09-25 04:25:50 +0530

    first commit

---
+++ myportfolio/src/components/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { SiCodersrank } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+export default function navbar() {
+  return (
+    <div>
+      <nav className='flex mt-6 ml-10 text-2xl justify-center'>
+        <SiCodersrank className='text-amber-200' />
+        <p className='text-[#f7f7f7] italic font-bold '>Abhishek</p>
+        <div className=''>
+          <ul className='flex gap-5 ml-70 '>
+            <li>About</li>
+            <li>Experience</li>
+            <li>Project</li>
+            <li>Skills</li>
+            <li>Contact</li>
+
+          </ul>
+        </div>
+        
+        <div className='ml-100 flex gap-4'>
+          <FaGithub />
+          <FaLinkedin />
+
+        </div>
+      </nav>
+
+    </div>
+  )
+}
