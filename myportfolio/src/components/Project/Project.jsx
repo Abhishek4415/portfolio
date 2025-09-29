115090de807e65fd27ecad5c9cc6d48ccc5199e7
Abhishek <siyaabhi441@gmail.com>
2025-09-29 23:32:16 +0530

    add

--- myportfolio/src/components/Project/Project.jsx
+++ myportfolio/src/components/Project/Project.jsx
@@ -16,7 +16,7 @@ export default function
 
       </div>
 
-      {/* all projects detail */}
+      {/* proejcts grid*/}
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-around '>
 
