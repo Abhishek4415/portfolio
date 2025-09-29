989405ced840e7d92e1244b6a59c791e06e3290c
Abhishek <siya@gmail.com>
2025-09-29 21:18:32 +0530

    proeject.jsx

---
+++ myportfolio/src/components/Project/Project.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function
+  Project() {
+  return (
+    <section
+    className=''
+    >
+      <div className="text-center mb-8">
+        <h2 className="text-3xl sm:text-4xl font-bold text-white">PROJECT</h2>
+        <div className="w-24 h-1 bg-[#8245ec] mx-auto mt-2"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          A collection of my project that i done during my btech.
+        </p>
+      </div>
+    </section>
+  )
+}
